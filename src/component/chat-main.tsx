a3ad48c229c7e8f93b3206c529a67cb53dcb00d0
LeeYongYun <yj9334@naver.com>
2023-09-01 16:08:00 +0900

    로그인 기능 구현

--- src/component/chat-main.tsx
+++ src/component/chat-main.tsx
@@ -5,29 +5,48 @@ import chatCore,{ responseProps } from "@/core/chat-core";
 import "../css/main.css";
 import ChatPhone from "./chat-phone";
 import {alert} from "../core/alert"
+import { useRef } from "react";
 
 const ChatMain = () =>{
-  
+  const userId = useRef<null | HTMLInputElement>(null);
+  const loginButton = useRef<null | HTMLButtonElement>(null);
+  const logoutButton = useRef<null | HTMLButtonElement>(null);
+
     const login = ()=>{
-        chatCore.axios("post","/login",{} ,(result: responseProps)=>{
+      if(userId.current && userId.current.value){
+        chatCore.axios("post","/login",{userId:userId.current.value} ,(result: responseProps)=>{
           if (result) {
             if(result.data.pageSession == 'Y'){
               window.sessionStorage.setItem("sessionYn",result.data.pageSession);
               alert("로그인에 성공했습니다.");
+              if(userId.current &&  loginButton.current && logoutButton.current){
+                userId.current.style.display = "none";
+                userId.current.value = "";
+                loginButton.current.style.display = "none";
+                logoutButton.current.style.display = "";
+              }
             }else{
               alert("이미 로그인이 되어있습니다.")
             }
           }
         })
-      };
-      const logout = ()=>{
+      }else{
+        alert("ID를 입력해주세요");
+      }
+    };
+    const logout = ()=>{
         chatCore.axios("post","/logout",{} ,(result: responseProps)=>{
           if (result) {
             window.sessionStorage.setItem("sessionYn","");
             alert("로그아웃에 성공했습니다.");
+            if(userId.current &&  loginButton.current && logoutButton.current){
+              userId.current && (userId.current.style.display = "");
+              loginButton.current.style.display = "";
+              logoutButton.current.style.display = "none";
+            }
           }
         })
-      };
+    };
     return (
         <div className = "scroll-snap-card">
             <div className = "contents">
@@ -45,8 +64,9 @@ const ChatMain = () =>{
             </div>
             <ChatPhone></ChatPhone>
             </div>
-            <button onClick={() =>login()}>로그인</button>
-            <button onClick={() =>logout()}>로그아웃</button>
+            <button ref={loginButton} onClick={() =>login()}>로그인</button>
+            <input ref={userId} type="text"></input>
+            <button ref={logoutButton} style={{display:"none"}}onClick={() =>logout()}>로그아웃</button>
             </div>
         </div>
     )
